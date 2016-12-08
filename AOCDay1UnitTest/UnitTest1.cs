7047b8de42995cdcb86fecc4c6fb9a775f5cd251
Antun Safaric <crosafan@gmail.com>
2016-12-08 19:28:52 +0100

    Finished Day1 part1

---
+++ AOCDay1UnitTest/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode;
+
+namespace AOCDay1UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Example1Test()
+        {
+            Mapper map = new Mapper();
+            Assert.AreEqual(5, map.GetBlocks("R2,L3"));
+        }
+
+        [TestMethod]
+        public void Example2Test()
+        {
+            Mapper map = new Mapper();
+            Assert.AreEqual(2, map.GetBlocks("R2,R2,R2"));
+        }
+
+        [TestMethod]
+        public void Example3Test()
+        {
+            Mapper map = new Mapper();
+            Assert.AreEqual(12, map.GetBlocks("R5,L5,R5,R3"));
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,15 +1,88 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace AdventOfCode
 {
-    class Program
+    public enum Direction
     {
-        static void Main(string[] args)
+        North,
+        East,
+        South,
+        West
+    }
+
+    public class Mapper
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Direction direction = Direction.North;
+
+        public Mapper()
         {
+            X = 0;
+            Y = 0;
+        }
+
+        public int GetBlocks(string input)
+        {
+            string[] moves = Array.ConvertAll(input.Split(','), x => x.Trim());            
+            foreach (var move in moves)
+            {
+
+                string turn = move[0].ToString();
+                int steps = Convert.ToInt32(move.Substring(1, move.Length - 1));
+
+                switch (turn)
+                {
+                    case "L":
+                        if (direction == Direction.North)
+                            direction = Direction.West;
+                        else direction--;
+                        break;
+
+                    case "R":
+                        if (direction == Direction.West)
+                            direction = Direction.North;
+                        else direction++;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                switch (direction)
+                {
+                    case Direction.North:
+                        this.Y += steps;
+                        break;
+                    case Direction.South:
+                        this.Y -= steps;
+                        break;
+                    case Direction.East:
+                        this.X += steps;
+                        break;
+                    case Direction.West:
+                        this.X -= steps;
+                        break;
+
+                }
+
+            }
+            return Math.Abs(this.X) + Math.Abs(this.Y);
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Mapper x = new Mapper();
+            StreamReader sr = new StreamReader(@"C:\Users\Antun\Documents\Visual Studio 2015\Projects\AdventOfCode\AdventOfCode\input.txt");
+            string input = sr.ReadLine();
+
+            Console.WriteLine(x.GetBlocks(input));
+
+            Console.Read();
         }
     }
 }
