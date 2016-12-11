1ebb1eddcae91241eb3e519adba0349e65c917f6
Antun Safaric <crosafan@gmail.com>
2016-12-11 23:00:34 +0100

    Finished AOCDay5Part2

--- AdventOfCodeDay5/Program.cs
+++ AdventOfCodeDay5/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -28,29 +30,51 @@ namespace AdventOfCodeDay5
 
         private static void Main(string[] args)
         {
-            string input = "ojvtpuvg";
+            string input = "abc";
 
-            int index = 0;
-            int count = 0;
-            string code = "";
+            int index = 3231900;
+            string[] code = { "*", "*", "*", "*", "*", "*", "*", "*" };
+
             string inputHash = "";
+            List<int> positions = new List<int>();
+            int counter = 0;
             using (MD5 md5Hash = MD5.Create())
             {
-                while (count < 8)
+                while (code.Contains("*"))
                 {
                     inputHash = input + index.ToString();
                     string hash = GetMd5Hash(md5Hash, inputHash);
                     string fiveDigit = hash.Substring(0, 5);
-                    if (fiveDigit == "00000")
+                    int position;
+                    // = Convert.ToInt32(hash[5].ToString());
+                    bool parsed = Int32.TryParse(hash[5].ToString(), out position);
+
+                    if (fiveDigit == "00000" && parsed)
                     {
-                        Console.WriteLine(hash);
-                        code += hash[5];
-                        count++;
+                        if (positions.Contains(position) && position < 8 && position >= 0)
+                        {
+                            index++;
+                            break;
+                        }
+                        else if (position < 8 && position >= 0)
+                        {
+                            positions.Add(position);
+                            code[position] = hash[6].ToString();
+                            index++;
+                            Console.WriteLine(hash);
+                            counter++;
+                            if (counter == 6) break;
+                        }
                     }
                     index++;
                 }
             }
-            Console.WriteLine(code);
+            var nekaj = "";
+            foreach (var codeChar in code)
+            {
+                nekaj += codeChar;
+            }
+            Console.WriteLine(nekaj);
             Console.Read();
         }
     }
