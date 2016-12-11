21d6ced5379b2704bc5346951147b3a53e6dba85
Antun Safaric <crosafan@gmail.com>
2016-12-11 18:51:38 +0100

    Finished AOCDay4Part1

---
+++ AOCDay4UnitTest/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCodeDay4;
+
+namespace AOCDay4UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckingIfRoomIsCorrect1()
+        {
+            Assert.AreEqual("abxyz", Solver.ReturnCode("aaaaa-bbb-z-y-x"));
+        }
+        [TestMethod]
+        public void CheckingIfRoomIsCorrect2()
+        {
+            Assert.AreEqual("abcde", Solver.ReturnCode("a-b-c-d-e-f-g-h"));
+        }
+        [TestMethod]
+        public void CheckingIfRoomIsCorrect3()
+        {
+            Assert.AreEqual("oarel", Solver.ReturnCode("not-a-real-room"));
+        }
+        [TestMethod]
+        public void CheckingIfRoomIsFalse1()
+        {
+            Assert.AreNotEqual("decoy", Solver.ReturnCode("totally-real-room"));
+        }
+        [TestMethod]
+        public void CheckingIfCheckSumIsCorrect1()
+        {
+            Assert.AreEqual("abxyz", Solver.GetCheckSum("aaaaa-bbb-z-y-x-123[abxyz]"));
+        }
+        [TestMethod]
+        public void CheckingIfCheckSumIsCorrect2()
+        {
+            Assert.AreEqual("abcde", Solver.GetCheckSum("a-b-c-d-e-f-g-h-987[abcde]"));
+        }
+        [TestMethod]
+        public void CheckingIfRoomIdIsCorrect1()
+        {
+            Assert.AreEqual("987", Solver.GetRoomId("a-b-c-d-e-f-g-h-987[abcde]"));
+        }
+        [TestMethod]
+        public void CheckingIfRoomIdIsCorrect2()
+        {
+            Assert.AreEqual("123", Solver.GetRoomId("aaaaa-bbb-z-y-x-123[abxyz]"));
+        }
+    }
+}
---
+++ AdventOfCodeDay4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeDay4
+{
+    public static class Solver
+    {
+        public static string ReturnCode(string input)
+        {
+            Regex RoomNameMatch = new Regex(@"^[A-z-]+");
+            string room = RoomNameMatch.Match(input).ToString();
+
+
+            char[] roomChars = room.ToCharArray();
+            List<char> distinctChars = roomChars.Distinct().Except(new[] { '-' }).ToList();
+            Dictionary<char, int> occurrences = distinctChars.ToDictionary(c => c,
+                                                                           c => roomChars.Count(x => x.Equals(c)));
+            return new string(
+                       occurrences.OrderByDescending(x => x.Value)
+                                  .ThenBy(x => x.Key)
+                                  .Select(x => x.Key)
+                                  .Take(5)
+                                  .ToArray());
+        }
+
+
+        public static string GetCheckSum(string input)
+        {
+            Regex CheckSumMatch = new Regex(@"(?<=\[).+?(?=\])");
+            return CheckSumMatch.Match(input).ToString();
+
+
+        }
+
+        public static string GetRoomId(string input)
+        {
+            Regex RoomIdMatch = new Regex(@"(\d+)");
+            return RoomIdMatch.Match(input).ToString();
+        }
+
+    }
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            string path = @"C:\Users\Antun\Documents\Visual Studio 2015\Projects\AdventOfCode\AdventOfCodeDay4\input.txt";
+            StreamReader sr = new StreamReader(path);
+            while (!sr.EndOfStream)
+            {
+                string input = sr.ReadLine();
+                if (Solver.ReturnCode(input) == Solver.GetCheckSum(input))
+                {
+                    sum += Convert.ToInt32(Solver.GetRoomId(input));
+                }
+            }
+            Console.WriteLine(sum);
+           
+            Console.Read();
+        }
+    }
+}
